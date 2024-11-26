e46ea6a16679e9edfbc3a7aea369fbf567c0cd38
devarumi <devarumi24@gmail.com>
2024-11-25 22:38:31 -0500

    edit user

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -5,16 +5,44 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index(Request $request)
+{
+    $users = User::with('userType')->get();
+
+    if ($request->ajax()) {
+        return DataTables::of($users)
+            ->addColumn('actions', function ($user) {
+                return '
+                    <div class="dropdown">
+                        <button class="btn btn-primary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-bars"></i>                        
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <button class="dropdown-item btnEditar" id="' . $user->id . '"><i class="fas fa-edit"></i>Editar</button>
+                            <form action="' . route('admin.users.destroy', $user->id) . '" method="POST" class="frmEliminar d-inline">
+                                ' . csrf_field() . method_field('DELETE') . '
+                                <button type="submit" class="dropdown-item btnEliminar" id="' . $user->id . '"><i class="fas fa-trash"></i> Eliminar</button>
+                            </form>
+                        </div>
+                    </div>';
+            })
+            ->addColumn('usertype_id', function ($user) {
+                return $user->userType->name;  // Aquí retornas el nombre del tipo de usuario
+            })
+            ->addColumn('zone_id', function ($user) {
+                return $user->zone_id->name;  // Aquí retornas el nombre de la  zona
+            })
+            ->rawColumns(['actions'])
+            ->make(true);
+    } else {
+        return view('admin.users.index', compact('users'));
     }
+}
+
 
     public function filterByUsertype($usertypeId)
     {
